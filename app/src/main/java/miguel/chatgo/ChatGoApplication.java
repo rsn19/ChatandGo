4dd60a47e3704819aff8aa28707e8ba8f9bafa3e
Miguel Halys <mhalys91@gmail.com>
2015-12-19 23:46:59 +0100

    Segunda entrega: paso 17

---
+++ app/src/main/java/miguel/chatgo/ChatGoApplication.java
@@ -0,0 +1,24 @@
+package miguel.chatgo;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Miguel on 12/19/2015.
+ */
+public class ChatGoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+    }
+
+}
--- app/src/main/java/miguel/chatgo/SignUpActivity.java
+++ app/src/main/java/miguel/chatgo/SignUpActivity.java
@@ -1,13 +1,27 @@
 package miguel.chatgo;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText usernameField, passwordField, emailField;
+    private ParseUser parseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +29,64 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        parseUser= new ParseUser();
+
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+        emailField = (EditText) findViewById(R.id.emailField);
     }
 
+    public void registrarse(View v) {
+        if(checkeoCampos()){
+            Toast.makeText(this, "Registro", Toast.LENGTH_LONG).show();
+            parseUser.setUsername(usernameField.getText().toString());
+            parseUser.setPassword(passwordField.getText().toString());
+            parseUser.setEmail(emailField.getText().toString());
+
+            parseUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e==null) {
+                        Intent intent = new Intent(getApplication(),MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        startActivity(intent);
+                    }else{
+                        generarDialogo(e.getMessage()).show();
+                    }
+                }
+            });
+        }else{
+            generarDialogo("Introduce los campos").show();
+
+        }
+    }
+
+
+    public boolean checkeoCampos() {
+        if (!usernameField.getText().toString().equals("") && !passwordField.getText().toString().equals("") && emailValidator(emailField.getText().toString()))
+            return true;
+
+        return false;
+    }
+
+
+    private Dialog generarDialogo(String error) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+        builder.setMessage(error);
+        builder.setTitle("Atención");
+        builder.setCancelable(true);
+
+        return builder.create();
+
+    }
+
+    public boolean emailValidator(String email) {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }
